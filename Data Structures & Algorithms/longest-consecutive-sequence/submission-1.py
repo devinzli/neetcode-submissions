8202e74dcd9eab2797ce04195cd4e90a5b188523
Zhen Li <devinzhenli@gmail.com>
2026-07-05 16:59:35 -0700

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    # union find
+    def longestConsecutive(self, nums: List[int]) -> int:
+        res = 0
+        mp = defaultdict(int)
+        for n in nums:
+            if not mp[n]:
+                mp[n] = mp[n-1] + mp[n+1] + 1
+                mp[n - mp[n-1]] = mp[n]
+                mp[n + mp[n+1]] = mp[n]
+                res = max(mp[n],res)
+        return res
